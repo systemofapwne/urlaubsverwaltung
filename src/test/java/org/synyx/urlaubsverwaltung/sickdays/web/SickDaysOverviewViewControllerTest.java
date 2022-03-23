8663bf57562eedcf9a1f09631f7999b2c147411f
honnel <github@honnel.de>
2022-03-24 00:21:03 +0100

    Remove duplicated fields from PersonBasedata domain
    * Add validation
    * Improve docs, scoping

--- src/test/java/org/synyx/urlaubsverwaltung/sickdays/web/SickDaysOverviewViewControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/sickdays/web/SickDaysOverviewViewControllerTest.java
@@ -64,7 +64,7 @@ class SickDaysOverviewViewControllerTest {
 
     @BeforeEach
     void setUp() {
-        sut = new SickDaysOverviewViewController(sickNoteService, personService, workDaysCountService,
+        sut = new SickDaysOverviewViewController(sickNoteService, personService, personBasedataService, workDaysCountService,
             new DateFormatAware(), clock);
     }
 
